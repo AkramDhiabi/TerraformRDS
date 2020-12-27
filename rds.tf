c6b1649233ae556cad87d1c7499e72ae7a770c4d
Akram Dhiabi <dhiabi.akram@gmail.com>
2020-12-27 18:06:20 +0100

    second change

--- rds.tf
+++ rds.tf
@@ -1,30 +1,12 @@
-module "db" {
-  source  = "terraform-aws-modules/rds/aws"
-  version = "~> 2.0"
-
-  identifier = "demodb"
-
-  engine            = "mysql"
-  engine_version    = "5.7.19"
-  instance_class    = "db.t2.micro"
-  allocated_storage = 10
-
-  name     = "demodb"
-  username = "akd"
-  password = "akddhib1985"
-  port     = "3306"
-
-  #iam_database_authentication_enabled = true
-
-  #vpc_security_group_ids = ["sg-12345678"]
-
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
-   
-  # Snapshot name upon DB deletion
-  final_snapshot_identifier = "demodb"
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
+resource "aws_db_instance" "mydb" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot =  true
 }
